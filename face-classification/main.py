5b852223f40c1a4ad83f0a90cfbd888b0b88654f
d0rb <hacperu2010@gmail.com>
2022-06-22 20:11:53 -0500

    update transfer learning
    cement the fact that we are to train the classifier parameters only !

--- face-classification/main.py
+++ face-classification/main.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
 
     criterion = nn.BCELoss()  # loss
 
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(model.classifier.parameters(), lr=0.001, momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
     model = train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs=100)
